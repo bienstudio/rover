a5bfa1640da85adf497a5ab1dcd2bc907cdafb55
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2017-03-30 09:48:24 -0400

    add specs for response helpers.

--- spec/lib/rover/helpers/response_spec.rb
+++ spec/lib/rover/helpers/response_spec.rb
@@ -1,4 +1,18 @@
+class DummyClass
+end
+
 describe Rover::Helpers::Response do
+  let(:klass) do
+    dummy = DummyClass.new
+    dummy.extend(Rover::Helpers::Response)
+    dummy
+  end
+
   describe '#rabl' do
+    it 'calls the Rabl render method' do
+      expect(Rabl).to receive(:render).with('foo', 'bar')
+
+      klass.rabl('foo', 'bar')
+    end
   end
 end
